0ae8f359ff0d6f3e3305e4623459ca568da2245a
OleksiiVTS <119164270+OleksiiVTS@users.noreply.github.com>
2023-06-28 14:20:57 +0300

    +

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,6 +12,7 @@ class Phonebook extends Component {
       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      { id: 'id-5', name: 'Adfds Gepeland', number: '227-91-26' },
     ],
     filter: '',
   };
